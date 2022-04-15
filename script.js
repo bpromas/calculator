8e8158991c8b2f8f095954b7b63066103fbf848d
bpromas <bpromas@gmail.com>
2022-04-15 01:05:36 -0300

    Create operate function

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+function operate(operator, a, b){
+    switch (operator) {
+        case "+":
+            add(a,b);
+            break;
+        case "-":
+            subtract(a,b);
+            break;
+        case "*":
+            multiply(a,b);
+            break;
+        case "/":
+            divide(a,b);
+            break;    
+        default:
+            break;
+    }
+}
+
 function add(a, b){
     return a + b;
 }
